49e8fb0b1e79c17e815a7fa2d58907f449d33ff8
akarshitm <122662804+akarshitm@users.noreply.github.com>
2025-09-30 18:16:37 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 locals {
   region        = "us-east-1"
-  name          = "Natwest-cluster"
+  name          = "EKS-cluster"
   vpc_cidr      = "172.31.0.0/16"
   azs           = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["172.31.1.0/24", "172.31.2.0/24"]
